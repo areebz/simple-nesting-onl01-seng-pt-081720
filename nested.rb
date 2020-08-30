5ddd1c03a1e78bd6e9f4e1984ef8bb8245360bca
Areeba Zahir <areebazahir3@gmail.com>
2020-08-30 04:36:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,7 +1,6 @@
-
 def hopper
-	programmer_hash = 
- 		{
+  programmer_hash = 
+    {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -16,6 +15,7 @@ def hopper
         }
      }
 
+ programmer_hash[:grace_hopper]
 
 end
 
